79c2ce3ad959b9e1e4037ce845de63a46860107f
Jins <m17376757163@163.com>
2026-03-11 16:59:12 +0800

    msg -s 'ref创建对象式相应数据'

--- 备份/05_reactive创建对象类型的响应式数据/src/components/Person.vue
+++ 备份/05_reactive创建对象类型的响应式数据/src/components/Person.vue
@@ -13,28 +13,28 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue";
+import { reactive } from "vue";
 defineOptions({
   name: "Person",
 });
 // JS //TS
-let car = ref({
+let car = reactive({
   name: "奔驰",
   price: 1000000,
 });
-let games = ref([
+let games = reactive([
   { id: "123132123", name: "王者荣耀", price: 0 },
   { id: "123132124", name: "英雄联盟", price: 1 },
 ]);
 function changeCar() {
-  car.value.name = "宝马";
-  car.value.price = 800000;
+  car.name = "宝马";
+  car.price = 800000;
 }
 
 function changeGames() {
-  games.value[0].name = "和平精英";
-  games.value[0].price = 0;
-  games.value[1].name = "英雄联盟手游";
+  games[0].name = "和平精英";
+  games[0].price = 0;
+  games[1].name = "英雄联盟手游";
   games[1].price = 1;
 }
 </script>
